8a5aea3d92c6bba65aad82fe73a124492f2d451a
akash <akash.testvagrant@embibe.com>
2019-10-03 10:49:14 +0530

    add the code to defind the annotation

---
+++ src/test/java/com/okcredit/tests/annotation/Priority.java
@@ -0,0 +1,15 @@
+package com.okcredit.tests.annotation;
+
+public enum Priority {
+    HIGH(1), MEDIUM(2),LOW(3);
+
+    private int rank;
+    Priority(int rank)
+    {
+        this.rank=rank;
+    }
+
+    public int getRank(){
+        return rank;
+    }
+}
